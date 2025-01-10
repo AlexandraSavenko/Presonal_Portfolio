a47af8ca508977cc90dff7f1b94a5f4ea022a05e
Sasha Savenko <sashasavenko3@gmail.com>
2025-01-10 12:57:57 +0200

    techDoc page added

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function sumSquare (arr) {
-    return arr.map(el => el * el).reduce((acc, el) => acc + el, 0)
+function sum (arr) {
+    return arr.reduce((acc, el) => {
+        if(el === true){
+   
+            return acc + 1}
+        return acc}, 0)
 }
-console.log(sumSquare([2, 5]))
+// function sum (arr){
+//     let acc = 0;
+//     arr.map(el => {if(el === true){acc += 1}})
+//     return acc
+// }
+console.log(sum([true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true]))
