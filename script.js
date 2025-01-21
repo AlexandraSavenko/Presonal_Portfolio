408955368fe0cdb59243294105490d5897602d43
Sasha Savenko <sashasavenko3@gmail.com>
2025-01-21 20:32:56 +0200

    cashReg added still doesn't work though

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const stringToNumber = function(str){
-    const result = parseInt(str)
-    console.log(typeof result)
-      return null;
-    }
-    console.log(stringToNumber("1234"))
+function maxProduct(numbers, size){
+const sortedArr = numbers.sort((a, b) => b - a).slice(0, size)
+let acc = 1;
+sortedArr.map(el => acc *= el)
+return acc
+}
+console.log(maxProduct([4, 3, 5], 2))
+console.log(maxProduct([10, 8, 7, 9], 3))
