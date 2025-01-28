157d2f6263b43d217f7cc3bcdcfe5d3af047a518
Sasha Savenko <sashasavenko3@gmail.com>
2025-01-28 14:02:17 +0200

    cashReg counts now

--- cashRegProj/cashReg.js
+++ cashRegProj/cashReg.js
@@ -37,6 +37,10 @@ const cashInDrawer = cid.reduce((acc, el)=> {
 return parseFloat(cashInDrawer.toFixed(2))
 }//end of countCashInDrawer
 
+function createRestMessage ({status, change}) {
+    changeInfo.textContent = `Status: ${status} ${change.flat().join(" ")}`
+}
+
 function purchase (){
     const result = {status: "",
         change: []
@@ -44,7 +48,10 @@ function purchase (){
     let cashInDrawer = countCashInDrawer();//335.41  (354.91)
   inputValue = Number(input.value);
 
-  rest = parseFloat((inputValue - price).toFixed(2));
+  rest = inputValue - price;
+
+rest === cashInDrawer ? result.status = "CLOSED" : result.status = "OPEN"
+
    if(inputValue < price){
     alert("Customer does not have enough money to purchase the item")
   return;
@@ -63,7 +70,6 @@ for (let i = currencyUnits.length - 1; i >= 0; i--){
   let currencyUnitName = currencyUnits[i][0];
   let currencyUnit = currencyUnits[i][1]
   let currencyInDrawer = cid[i][1]
-  console.log(currencyUnitName)
   if(rest >= currencyUnit && currencyInDrawer > 0){
  let amountOfUnits = 0;
 
@@ -75,22 +81,21 @@ for (let i = currencyUnits.length - 1; i >= 0; i--){
     amountOfUnits += currencyUnit;
     cashInDrawer -= currencyUnit;
   }//end of while loop
-  console.log(amountOfUnits)
   if(amountOfUnits > 0){
 result.change.push([currencyUnitName, parseFloat(amountOfUnits.toFixed(2))])
   }
 
   }
 }//end of for loop
-cashInDrawer.toFixed(2) === 0 ? result.status = "OPEN" : result.status = "CLOSED"
-console.log(result)
+
 
 let formText = (status, changeArr) => changeArr.map(([unit, amount]) => `Status: ${status} ${unit}: $${Number(amount)}`).join(" ")
 
 //const resultText = formText(cashInDrawer? "OPEN" : "CLOSE", changeArr)
-
+createRestMessage(result)
 }
   
 purchaseBtn.addEventListener("click", ()=> {
     purchase()
 })
+//console.log(cashInDrawer.toFixed(2) === 0)
